4888fea33a1c2987c5b45156c4c580b8fdc63f57
dadras <dadras.moh@gmail.com>
2024-05-26 20:39:32 +0330

    first-commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import "./App.css"
+import logo from "./assets/images/pic1.jpg"
+export default function App() {
+
+
+  return (
+
+    <>
+      <div className="">
+
+        <ul className="nav">
+          <li>{"آخرین دوره ها"}</li>
+          <li>{"تخفیفات روزانه"}</li>
+          <li>{"آخرین دوره ها"}</li>
+          <li>{"آخرین دوره ها"}</li>
+          <li>{"آخرین دوره ها"}</li>
+          <li>{"آخرین دوره ها"}</li>
+          <li>{"آخرین دوره ها"}</li>
+          <li>{"آخرین دوره ها"}</li>
+        </ul>
+
+        <div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <img src={logo} />
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <img src={logo} />
+
+        </div>
+
+
+
+
+
+
+        <img src={logo} />
+
+      </div>
+    </>
+
+  )
+}
+
